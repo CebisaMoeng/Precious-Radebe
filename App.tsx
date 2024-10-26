9d280eb0ac7168d66ce0290883097688a9292dbb
Cebisa Moeng <ST10455177@imconnect.edu.za>
2024-10-26 17:13:45 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './HomeScreen';
+import CourseDetail from './CourseDetail';
+import Calculate from './Calculate';
+import Signup from './Signup';
+import Login from './Login';
 
-export default function App() {
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+  Calculate: undefined;
+  Contact: undefined;
+  Signup: undefined;
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={CourseDetail} />
+        <Stack.Screen name="Calculate" component={Calculate} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
